651d3b2566eb359274f8fa18231cb8769edecfaf
wilfried <fournier.wilf@gmail.com>
2025-03-29 18:27:15 +0100

    Passing password by env

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,8 +11,19 @@ final class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+
+        $passwordApi = $this->getParameter('app.passwordapi');
+
+
+        /*$api = new \potibm\Bluesky\BlueskyApi('marbobley.bsky.social', '$passwordApi');
+        $postService = new \potibm\Bluesky\BlueskyPostService($api);
+        $post = \potibm\Bluesky\Feed\Post::create('✨ example mentioning @atproto.com to share the URL 👨‍❤️‍👨 https://en.wikipedia.org/wiki/CBOR.');
+        $response = $api->createRecord($post);*/
+
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'passwordApi' => $passwordApi,
         ]);
     }
 }
